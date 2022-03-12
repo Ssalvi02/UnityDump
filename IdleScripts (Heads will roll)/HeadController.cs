4d3df1b23cfb2af1ec6955b249a80fcc33086be8
sSalvi <89103599+Ssalvi02@users.noreply.github.com>
2022-03-11 23:02:01 -0300

    Controls the points

--- IdleScripts (Heads will roll)/HeadController.cs
+++ IdleScripts (Heads will roll)/HeadController.cs
@@ -6,18 +6,6 @@ public class HeadController : MonoBehaviour
 {
     [SerializeField] private SpinController sc;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "PointCollider")
